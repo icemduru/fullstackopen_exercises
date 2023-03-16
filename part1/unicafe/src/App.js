86c8fdd6125fb99542a755391b85b1b960c7dd05
icemduru <icemduru@gmail.com>
2023-03-16 13:09:34 +0200

    pushing ex1.8 and ex1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,10 +18,22 @@ const Title = (props) =>{
 
 const Stats = (props) =>{
   console.log(props)
-  return(
+  if (props.number1 > 0 || props.number2 > 0 || props.number3 > 0) {
+    return(
+      <div>
+        <p>{props.text1} {props.number1}</p>
+        <p>{props.text2} {props.number2}</p>
+        <p>{props.text3} {props.number3}</p>
+        <p>{props.text4} {props.number4}</p>
+        <p>{props.text5} {props.number5}</p>
+        <p>{props.text6} {props.number6}</p>
+      </div>
+    )
+  }
+  return (
     <div>
-      <p>{props.text} {props.number}</p>
-    </div>
+      <p>No feedback given.</p>
+    </div>    
   )
 }
 
@@ -59,12 +71,12 @@ const App = () => {
       <Button handleClick={giveNeutral} text="neutral" />
       <Button handleClick={giveBad} text="bad" />
       <Title text="Statistics" />
-      <Stats text="Good" number={good} />
-      <Stats text="Neutral" number={neutral} />
-      <Stats text="Bad" number={bad} />
-      <Stats text="All" number={total} />
-      <Stats text="average" number={average} />
-      <Stats text="positive" number={positive} />
+      <Stats text1="Good" number1={good}
+      text2="Neutral" number2={neutral}
+      text3="Bad" number3={bad}
+      text4="All" number4={total}
+      text5="average" number5={average}
+      text6="positive" number6={positive}  />
     </div>
   )
 }
