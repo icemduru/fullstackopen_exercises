ff2f8ab325473798e16e9e85a2d0f37f7920ee7d
icemduru <icemduru@gmail.com>
2023-03-21 15:39:07 +0200

    pushing for ex2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const NumberPrint = (props) => {
+  return(
+    <div>
+      {props.numbers.map(eachNumber =>
+        <p key={eachNumber.name}>{eachNumber.name}</p>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newName
+    }
+  
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <NumberPrint numbers={persons} />
+    </div>
+  )
+}
+
+export default App
