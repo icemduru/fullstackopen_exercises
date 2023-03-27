da5d6201884eaca272c3c2a1792a8ab5bf378f02
icemduru <icemduru@gmail.com>
2023-03-27 15:45:32 +0300

    pushing for ex2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -13,11 +13,14 @@ const NumberPrint = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 234 }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [newSearch, setnewSearch] = useState('')
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
@@ -47,9 +50,32 @@ const App = () => {
     }
   }
 
+  const handleSearch = (event) => {
+    setnewSearch(event.target.value)
+  }
+
+ const toShow = () => {
+  if (newSearch.length){
+    const newSearchlower = newSearch.toLowerCase()
+    console.log("newSearchlower log:",newSearchlower)
+    const newPersons = persons.filter(person => person.name.toLowerCase().includes(newSearchlower))
+    console.log("persons log", persons)
+    console.log("newPersons log", newPersons)
+    return (newPersons)
+  } else{
+    return(persons)
+  }
+ }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={newSearch} onChange={handleSearch} />
+        </div>
+      </form>
+      <h2> add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -61,7 +87,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <NumberPrint numbers={persons} />
+      <NumberPrint numbers={toShow()} />
     </div>
   )
 }
