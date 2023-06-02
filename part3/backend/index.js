5eb83d5fa0599abc976e437cbecb8d85ad7cf481
icemduru <icemduru@gmail.com>
2023-06-02 13:59:27 +0300

    ex3.15 and 3.16

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,9 +3,22 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-
-
 const app = express()
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
@@ -54,18 +67,24 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(String(request.params.id)).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(String(request.params.id))
+    .then(person => {
+      if (person) { 
       response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-    .catch(error => {response.status(404).end()})
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    console.log(`will delete person with id ${id}`)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
     response.status(204).end()
+  })
+  .catch(error => next(error))
 })
   
 app.post('/api/persons', (request, response) => {
@@ -92,6 +111,9 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
   })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
