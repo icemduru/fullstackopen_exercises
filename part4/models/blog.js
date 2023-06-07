d2ed7f25efeec7e7d33ea99745ea31eff742ed70
icemduru <icemduru@gmail.com>
2023-06-07 13:29:40 +0300

    starting part 4

---
+++ part4/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+    //console.log('result', result)
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+module.exports = mongoose.model('Blog', blogSchema)
