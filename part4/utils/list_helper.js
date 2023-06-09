ba682efbbe988cb998b2a50ac802bf464b82fb4c
icemduru <icemduru@gmail.com>
2023-06-09 14:32:04 +0300

    ex4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,21 @@ const totalLikes = (blogs) => {
   return likesArray.reduce((sum, item) => sum + item, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const max = Math.max(...blogs.map((eachBlog) => eachBlog.likes));
+  const findedFavorite = blogs.find((eachBlog) => eachBlog.likes === max);
+  if (findedFavorite) {
+    return {
+      title: findedFavorite.title,
+      author: findedFavorite.author,
+      likes: findedFavorite.likes,
+    };
+  }
+  return null;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
