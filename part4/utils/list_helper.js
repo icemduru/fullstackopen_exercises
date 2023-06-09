5d2aa144cd138ab79de3a2f407a8af02f040f013
icemduru <icemduru@gmail.com>
2023-06-09 12:04:56 +0300

    ex4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,14 @@
-const dummy = (blogs) => {return 1};
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  const likesArray = [];
+  blogs.forEach((eachBlog) => {
+    likesArray.push(eachBlog.likes);
+  });
+  return likesArray.reduce((sum, item) => sum + item, 0);
+};
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 };
